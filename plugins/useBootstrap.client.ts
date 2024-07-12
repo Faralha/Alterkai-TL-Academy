b76fda1576e7c20d34cfa674c29c4d1bd6719404
faralha <faralhaonly@gmail.com>
2024-07-13 00:31:49 +0700

    fix usebootstrap

--- plugins/useBootstrap.client.ts
+++ plugins/useBootstrap.client.ts
@@ -1,9 +1,7 @@
 // plugins/useBootstrap.client.ts
-import bootstrap from 'bootstrap';
+import { defineNuxtPlugin } from '#app';
+import * as bootstrap from 'bootstrap';
+
 export default defineNuxtPlugin(nuxtApp => {
-  return {
-    provide: {
-      bootstrap: bootstrap
-    }
-  }
+  nuxtApp.provide('bootstrap', bootstrap);
 });
